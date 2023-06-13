2fb91d8bce2f5b274ccbb69ea93410fca41baf95
TamariIv <taivgi@g.jct.ac.il>
2023-06-13 14:21:07 +0300

    add documentation

--- include/PriorityQueue.h
+++ include/PriorityQueue.h
@@ -4,7 +4,14 @@
 #include <list>
 #include <exception>
 
-
+/**
+ * @brief A template struct representing a comparator.
+ *
+ * The comparator provides a function to compare two elements of type T.
+ * The function subtracts b from a and returns the result.
+ *
+ * @tparam T The type of elements to be compared.
+ */
 template <typename T>
 struct MyComparator {
 	T operator()(T a, T b) const {
@@ -13,6 +20,14 @@ struct MyComparator {
 };
 
 
+/**
+ * @brief A template class for a priority queue.
+ *
+ * The priority queue stores elements of type T in a sorted order based on the provided comparator.
+ * Elements with lower priority are placed at the front of the queue.
+ *
+ * @tparam T The type of elements to be stored in the priority queue.
+ */
 template <typename T>
 class PriorityQueue {
    
@@ -27,7 +42,13 @@ private:
 };
 
 
-
+/**
+ * @brief Pushes an element into the priority queue.
+ *
+ * The element is inserted at the correct position in the sorted order.
+ *
+ * @param t The element to be pushed into the priority queue.
+ */
 template<typename T>
 inline void PriorityQueue<T>::push(const T& t)
 {
@@ -37,6 +58,13 @@ inline void PriorityQueue<T>::push(const T& t)
 	elements.insert(it, t);
 }
 
+
+/**
+ * @brief Retrieves and removes the highest priority element from the priority queue.
+ *
+ * @return The highest priority element from the priority queue.
+ * @throw std::runtime_error if the priority queue is empty.
+ */
 template<typename T>
 inline T PriorityQueue<T>::poll()
 {
